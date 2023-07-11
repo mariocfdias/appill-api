16206123121ea8bcf84ae42d9a54cef47671aed7
Mario Cesar <mariocesarfdias@gmail.com>
2023-07-11 07:57:19 -0300

    reverting whitelist in dto

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,7 @@ import { LogInterceptor } from './interceptors/log.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LogInterceptor());
   app.enableCors();
 
